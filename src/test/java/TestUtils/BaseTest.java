8434b65d02ea5fd48039940791f43ad83aab7070
amirul@posable.com.my <Magnet46!@>
2024-10-05 00:10:01 +0800

    new changes

--- src/test/java/TestUtils/BaseTest.java
+++ src/test/java/TestUtils/BaseTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Properties;
@@ -13,7 +14,11 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -32,23 +37,35 @@ public class BaseTest {
 		Properties prop = new Properties();
 		FileInputStream fis = new FileInputStream("C:\\Users\\posab\\OneDrive\\Documents\\POSABLE\\Documents\\Automation\\Interview\\ClaimHub\\ClaimHub\\src\\main\\java\\resources\\GlobalData.properties");
 		prop.load(fis);
-		String browserName = prop.getProperty("browser");
+		String browserName = System.getProperty("browser")!=null ? System.getProperty("browser") :prop.getProperty("browser");
+//		String browserName = prop.getProperty("browser");
 		
-		if(browserName.equalsIgnoreCase("chrome"))
+		if(browserName.contains("chrome"))
 		{
 		WebDriverManager.chromedriver().setup();
-		 driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		if(browserName.contains("headless"))
+		{
+		options.addArguments("headless");
+		}
+		 driver = new ChromeDriver(options);
 		
 		}
 		else if (browserName.equalsIgnoreCase("firefox"))
 		{
-			
+			WebDriverManager.firefoxdriver().setup();
+//			System.setProperty("webdriver.gecko.driver","C:\\Users\\posab\\Downloads\\geckodriver-v0.35.0-win-aarch64\\geckodriver.exe");
+			FirefoxOptions options = new FirefoxOptions();
+		    options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+			 driver = new FirefoxDriver();
 		}
 		else if (browserName.equalsIgnoreCase("edge"))
 		{
-			System.setProperty("webdriver.edge.driver","edge.exe");
+			WebDriverManager.edgedriver().setup();
+//			System.setProperty("webdriver.edge.driver","C:\\Users\\posab\\Downloads\\edgedriver_win64\\msedgedriver.exe");
 			 driver = new EdgeDriver();
 		}
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 		return driver;
 	}
@@ -85,7 +102,7 @@ public class BaseTest {
 	@AfterMethod(alwaysRun=true)
 	public void tearDown()
 	{
-		driver.close();
+//		driver.close();
 		driver.quit();
 	}
 
